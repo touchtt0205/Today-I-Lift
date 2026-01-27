076ae8123b2a8488663c63e3033168a423c919e5
touchtt0205 <tttouch1122@gmail.com>
2026-01-27 22:54:39 +0700

    feat(appbar/pages): add dynamic appbar and home, workout, play, calendar, profile pages
    (cherry picked from commit 9e8af09b43398d728f472b602b77266d41201205)

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -6,8 +6,10 @@ class TodayILiftApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
       title: 'Today I Lift',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
