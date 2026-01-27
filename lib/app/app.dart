9b3032a1f3a9e62d41498a8ceb37688deb0bf215
touchtt0205 <tttouch1122@gmail.com>
2026-01-27 17:14:27 +0700

    chore: add bottom navigation skeleton

---
+++ lib/app/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'app_scaffold.dart';
+
+class TodayILiftApp extends StatelessWidget {
+  const TodayILiftApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Today I Lift',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const AppScaffold(),
+    );
+  }
+}
