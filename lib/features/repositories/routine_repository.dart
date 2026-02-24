95bb17dabb29b13e989985d891c3c13b85d7387b
SUPATKN <yacht1400@gmail.com>
2026-02-24 20:05:44 +0700

    Feat ( routine ) : add routine page and connect with supabase
    (cherry picked from commit bae675cc2f60f3c5427d69c9b2cbf26c85ab1c11)

--- lib/features/repositories/routine_repository.dart
+++ lib/features/repositories/routine_repository.dart
@@ -19,19 +19,31 @@ class RoutineRepository {
     await _client.from('routines').delete().eq('id', id);
   }
 
-  Future<String> createRoutineReturnId(String name) async {
+  Future<String> createRoutineReturnId(String name, int icon, int color) async {
     final userId = _client.auth.currentUser!.id;
 
     final res = await _client
         .from('routines')
-        .insert({'user_id': userId, 'name': name})
+        .insert({'user_id': userId, 'name': name, 'icon': icon, 'color': color})
         .select()
         .single();
 
     return res['id'];
   }
 
-  Future<void> updateRoutine(String id, String name) async {
-    await _client.from('routines').update({'name': name}).eq('id', id);
+  Future<void> updateRoutine(
+    String id,
+    String name,
+    int? icon,
+    int? color,
+  ) async {
+    await _client
+        .from('routines')
+        .update({
+          'name': name,
+          if (icon != null) 'icon': icon,
+          if (color != null) 'color': color,
+        })
+        .eq('id', id);
   }
 }
